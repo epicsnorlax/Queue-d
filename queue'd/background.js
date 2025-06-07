9a2a033c9a00c31b0a5580e12313b9da8545e6d6
Yash Kalani <yash@kalani.name>
2025-06-06 22:19:40 -0400

    Delete queue'd directory

--- queue'd/background.js
+++
@@ -1,6 +0,0 @@
-// background.js
-// This runs in the background (service worker) of your extension
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed!');
-});
--- queue'd/content.js
+++
@@ -1,5 +0,0 @@
-// content.js
-// This runs in the context of every web page (if enabled in manifest)
-
-console.log('Content script loaded!');
-// You can add your DOM-manipulating code here
